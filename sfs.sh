528c52efbe44d7ca58d0e69d4960d1a070cebd9b
gaushi <shirsekar.1@osu.edu>
2017-06-01 13:57:01 +0200

    Update sfs.sh

--- sfs.sh
+++ sfs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #ancestral genome used is Arabidopsis lyrata
 #finding a global estimate of SFS
+#-p option tells number of threads to use. Here I have used 24 threads
 angsd -bam pool1_alyr_bamlist -doSaf 1 -anc /ebio/abt6_projects9/Methylome_variation_HPG1/RAD15/data/refgenome/Arabidopsis_lyrata.v.1.0.dna_rm.toplevel.fa -GL 1 -P 24 -out MISJ.alyr
